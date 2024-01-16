0f34a28147fac69a39fcaaea0e9a6afcfec2793c
Yan <yanyakovtsev@gmail.com>
2024-01-16 19:05:37 +0300

    create services layer

--- internal/models/person.go
+++ internal/models/person.go
@@ -1,10 +1,14 @@
 package models
 
+import "time"
+
 type Person struct {
-	Name        string `db:"name"`
-	Surname     string `db:"surname"`
-	Patronymic  string `db:"patronymic"`
-	Age         int8   `db:"age"`
-	Gender      string `db:"gender"`
-	Nationality string `db:"nationality"`
+	ID          string    `db:"id"`
+	Name        string    `db:"name"`
+	Surname     string    `db:"surname"`
+	Patronymic  string    `db:"patronymic"`
+	Age         int8      `db:"age"`
+	Gender      string    `db:"gender"`
+	Nationality string    `db:"nationality"`
+	CreatedAt   time.Time `db:"created_at"`
 }
---
+++ internal/services/person.go
@@ -0,0 +1,73 @@
+package services
+
+import (
+	"context"
+	"time"
+
+	"github.com/HeadGardener/effective_mobile/internal/models"
+	"github.com/google/uuid"
+)
+
+type PersonStorage interface {
+	Save(ctx context.Context, person *models.Person) (string, error)
+	GetByID(ctx context.Context, id string) (*models.Person, error)
+	Get(ctx context.Context, filters map[string]any, limit int) ([]models.Person, error)
+	Delete(ctx context.Context, id string) error
+	Update(ctx context.Context, person *models.Person) error
+}
+
+type PersonDataProvider interface {
+	GetAge(ctx context.Context, name string) (int8, error)
+	GetGender(ctx context.Context, name string) (string, error)
+	GetNationality(ctx context.Context, name string) (string, error)
+}
+
+type PersonService struct {
+	personStorage      PersonStorage
+	personDataProvider PersonDataProvider
+}
+
+func (s *PersonService) Create(ctx context.Context, person *models.Person) (string, error) {
+	age, err := s.personDataProvider.GetAge(ctx, person.Name)
+	if err != nil {
+		return "", err
+	}
+	person.Age = age
+
+	gender, err := s.personDataProvider.GetGender(ctx, person.Name)
+	if err != nil {
+		return "", err
+	}
+	person.Gender = gender
+
+	nationality, err := s.personDataProvider.GetNationality(ctx, person.Name)
+	if err != nil {
+		return "", err
+	}
+	person.Nationality = nationality
+
+	person.ID = uuid.NewString()
+	person.CreatedAt = time.Now()
+
+	return s.personStorage.Save(ctx, person)
+}
+
+func (s *PersonService) Get(ctx context.Context, filters map[string]any, limit int) ([]models.Person, error) {
+	return s.personStorage.Get(ctx, filters, limit)
+}
+
+func (s *PersonService) Delete(ctx context.Context, id string) error {
+	if _, err := s.personStorage.GetByID(ctx, id); err != nil {
+		return err
+	}
+
+	return s.personStorage.Delete(ctx, id)
+}
+
+func (s *PersonService) Update(ctx context.Context, person *models.Person) error {
+	if _, err := s.personStorage.GetByID(ctx, person.ID); err != nil {
+		return err
+	}
+
+	return s.personStorage.Update(ctx, person)
+}
